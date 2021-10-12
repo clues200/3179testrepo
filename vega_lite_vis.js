f40fa730306bed112f5bb86319fa714493486354
clues200 <91269509+clues200@users.noreply.github.com>
2021-10-13 00:18:09 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -6,6 +6,6 @@ vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 
 var vg_2 = "firstinteractive.vg.json";
 
-vegaEmbed("#bubble_plot", vg_1).then(function(result) {
+vegaEmbed("#bubble_plot", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
