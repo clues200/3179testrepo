1551ab0cc30b8d354b6f229744a6ea00105a13c5
clues200 <91269509+clues200@users.noreply.github.com>
2021-10-18 00:35:29 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -9,3 +9,33 @@ var vg_2 = "firstinteractive.vg.json";
 vegaEmbed("#bubble_plot", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "numfemalesmokers.vg.json";
+
+vegaEmbed("#numfemalesmokers", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "nummalesmokers.vg.json";
+
+vegaEmbed("#nummalesmokers", vg_4).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "numsmokers.vg.json";
+
+vegaEmbed("#numsmokers", vg_5).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "prescription.vg.json";
+
+vegaEmbed("#prescription", vg_6).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "smokingfat.vg.json";
+
+vegaEmbed("#smokingfat", vg_7).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
